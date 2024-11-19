bfc491c1511e55fdff5073b7773d5bd37caa60fa
Rana Ismael <hassnhassoon4@gmail.com>
2024-11-19 15:22:59 -0500

    TernaryOperatorDemo

---
+++ src/TernaryOperatorDemo.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class TernaryOperatorDemo {
+    public static void main(String[] args) {
+        int age = 18;
+        String result = age < 100 ?
+                "Less than 100" : "Greater than 100";
+        System.out.println(result); //Less than 100
+    }
+}
